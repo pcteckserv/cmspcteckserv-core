9b0541a602b6f37dd73596a5ed3c9cc0780f12a5
diogopcteckserv-prog <diogopcteckserv@gmail.com>
2026-08-24 15:35:36 +0100

    Add favicon link to admin layouts

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'Administração' }}</title>
+    <link rel="icon" href="{{ app(\Pcteckserv\CmsCore\Support\SiteOptions::class)->get('site_icon_url') }}">
     @vite([
         'vendor/pcteckserv/cms-core/resources/css/admin.scss',
         'vendor/pcteckserv/cms-core/resources/js/admin.js',
